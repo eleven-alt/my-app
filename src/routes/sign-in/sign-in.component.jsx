1691176ce423a68dbc95ff094bbc51fca95adb19
Anshu Bhadauria <anshu.bhadauria@ibm.com>
2023-05-21 16:38:40 +0530

    created sign up component, the form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
+//import { useEffect } from 'react';
+//import { getRedirectResult } from 'firebase/auth'
+
+import { signInWithGooglePopup, createUserDocumentFromAuth,} from '../../utils/firebase/firebase.utils';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 
 
 const SignIn = () => {
+    
+   
     const logGoogleUser = async () => {
         
-        const {user} = await signInWithGooglePopup();
+        const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
+    
 
     return(
         <div>
@@ -15,6 +22,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            < SignUpForm/>
         </div>
     )
 }
