77808920dc604324f9c3445ab8b5045341a3536d
Anshu Bhadauria <anshu.bhadauria@ibm.com>
2023-06-01 13:56:41 +0530

    changes

--- src/routes/Navigation/navigation.component.jsx
+++ src/routes/Navigation/navigation.component.jsx
@@ -1,16 +1,21 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { UserContext } from '../../contexts/user.context';
-import { CartContext } from '../../contexts/cart-context';
-
 import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/card-dropdown/card-dropdown.component';
-import { ReactComponent as CrwnLogo } from '../../assests/crown.svg';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import { NavigationContainer, NavLinks, NavLink, LogoContainer } from  './navigation.styles';
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -23,18 +28,14 @@ const Navigation = () => {
           <CrwnLogo className='logo' />
         </LogoContainer>
         <NavLinks>
-          <NavLink to='/shop'>
-            SHOP
-          </NavLink>
+          <NavLink to='/shop'>SHOP</NavLink>
 
           {currentUser ? (
             <NavLink as='span' onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
-            <NavLink  to='/auth'>
-              SIGN IN
-            </NavLink>
+            <NavLink to='/auth'>SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
